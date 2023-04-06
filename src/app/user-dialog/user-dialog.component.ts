29e71b1b0395f03a54e4686566f76a68fc7d90dd
your_username <patrizio.sbrozzi@gmail.com>
2023-04-06 13:05:58 +0200

    table and data ok

--- src/app/user-dialog/user-dialog.component.ts
+++ src/app/user-dialog/user-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from '../interfaces/user';
 
@@ -8,11 +8,24 @@ import { User } from '../interfaces/user';
   styleUrls: ['./user-dialog.component.scss']
 })
 export class UserDialogComponent {
+  dataTable : Array<any> = [
+    [1,2,3,4,5,6,7,8,9],
+    [10,20,30,40,50,60,70,80,90]
+  ]
+  test: string = "ciao"
   constructor(
     public dialogRef: MatDialogRef<UserDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: User,
   ) { }
-  onCancelUserDialog(): void {
-    this.dialogRef.close();
+  onCancelUserDialog( i : number): void {
+    console.log('click');
+    // this.dialogRef.close();
+    let elem = document.querySelectorAll('[class^="td-"]')
+    elem.forEach(item => {
+      // console.log(item.className.indexOf(`td-${i}`));
+      if(item.className.indexOf(`td-${i}`) >= 0) {
+        console.log(item.innerHTML);
+        }
+    });
   }
 }
